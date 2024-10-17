5fa4fe51d5b9f790e121f9029f9d0aeb85c70d23
prostodasha62@gmail.com <prostodasha62@gmail.com>
2024-10-17 20:17:40 +0500

    Task5

---
+++ Tyuiu.KorobeinikovaDD.Sprint3.Task5.V20/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Tyuiu.KorobeinikovaDD.Sprint3.Task5.V20.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнила: Коробейникова Д.Д. | ПКТб-24-1";
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* Тема: Оператор цикла while                                                  *");
+Console.WriteLine("* Задание #5                                                                  *");
+Console.WriteLine("* Вариант #20                                                                 *");
+Console.WriteLine("* Выполнила: Коробейникова Дарья Денисовна | ПКТб-24-1                        *");
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                    *");
+Console.WriteLine("* Написать программу на С#, используя вложенный цикл, которая вычисляет сумму *");
+Console.WriteLine("* суммы ряда по формуле                                                       *");
+Console.WriteLine("*                                                                             *");
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                            *");
+Console.WriteLine("*******************************************************************************");
+
+int x = 5;
+int startValue1 = 1;
+int stopValue1 = 3;
+int startValue2 = 1;
+int stopValue2 = 6;
+
+Console.WriteLine("Переменная x = " + x);
+Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                                  *");
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("Cумма суммы ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+Console.ReadKey();
